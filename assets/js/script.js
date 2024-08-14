2f7b72da59142eda6a6d7a85e4ec1a73a261a66b
Adam <aorf@femmeosage.net>
2024-08-13 20:31:06 -0500

    created a function to generate task id

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,35 +4,26 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+  const id = nextId;
+  nextId += 1;
+  localStorage.setItem("nextId", JSON.stringify(nextId));
+  return id;
 }
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
-
-}
+function createTaskCard(task) {}
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
-
-}
+function renderTaskList() {}
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-
-}
+function handleAddTask(event) {}
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
-}
+function handleDeleteTask(event) {}
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
-
-}
+function handleDrop(event, ui) {}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
-
-});
+$(document).ready(function () {});
