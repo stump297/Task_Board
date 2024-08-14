c0964bddc412fd0e867578e15eaedf8935e3faef
Adam <aorf@femmeosage.net>
2024-08-13 20:44:48 -0500

    created a function to create a task card.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Todo: create a function to generate a unique task id
+// create a function to generate a unique task id
 function generateTaskId() {
   const id = nextId;
   nextId += 1;
@@ -10,20 +10,37 @@ function generateTaskId() {
   return id;
 }
 
-// Todo: create a function to create a task card
+// create a function to create a task card
 function createTaskCard(task) {}
 
-// Todo: create a function to render the task list and make cards draggable
+const taskCard = $(`
+      <div class="task-card" data-id="${task.id}">
+        <h3>${task.title}</h3>
+        <p>${task.description}</p>
+        <p>Deadline: ${task.deadline}</p>
+        <button class="delete-task">Delete</button>
+      </div>
+    `);
+
+if (dayjs(task.deadline).isBefore(dayjs())) {
+  taskCard.css("background-color", "red");
+} else if (dayjs(task.deadline).isBefore(dayjs().add(3, "day"))) {
+  taskCard.css("background-color", "yellow");
+}
+
+return taskCard;
+
+// create a function to render the task list and make cards draggable
 function renderTaskList() {}
 
-// Todo: create a function to handle adding a new task
+// create a function to handle adding a new task
 function handleAddTask(event) {}
 
-// Todo: create a function to handle deleting a task
+// create a function to handle deleting a task
 function handleDeleteTask(event) {}
 
-// Todo: create a function to handle dropping a task into a new status lane
+// create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {}
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+// when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {});
